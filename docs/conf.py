0d5db7b312e95e008b0cb5df98f10964ad36a602
freesemt <freesemt@gmail.com>
2025-03-13 15:59:16 +0900

    change theme to awesome

--- docs/conf.py
+++ docs/conf.py
@@ -30,5 +30,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinxawesome_theme'
 html_static_path = ['_static']
+html_favicon = "_static/molamola.ico"
