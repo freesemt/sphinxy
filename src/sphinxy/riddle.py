f9a4189017f527c68e273a8253495106aa0315de
freesemt <freesemt@gmail.com>
2025-03-13 15:07:49 +0900

    Added docstrings

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -4,11 +4,14 @@ from typing import Iterator
 
 @dataclass(frozen=True)
 class Riddle:
+    """A class representing a riddle."""
     question: str
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """Check if the given answer is correct."""
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
+        """Get a hint for the riddle"""
         yield from iter(self.answer)
